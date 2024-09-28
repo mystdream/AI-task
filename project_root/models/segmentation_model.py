c6aa58772248db1f0563ce7509720f33e6ea816a
Abhay Sharma <119748194+mystdream@users.noreply.github.com>
2024-09-28 15:42:54 +0530

    Create segmentation_model.py

---
+++ project_root/models/segmentation_model.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+class SegmentationModel:
+    def __init__(self):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def segment_image(self, image):
+        outputs = self.predictor(image)
+        return outputs
